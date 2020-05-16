9976d4878d2b45737c26fca50ff23a48ec3d6607
Tauan Matos <tauan.matos164@gmail.com>
2020-05-16 16:03:37 -0300

    Add downloadURL and change version

--- Source/Tampermonkey/EnviarRecibo.js
+++ Source/Tampermonkey/EnviarRecibo.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Imprimir comprovante
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Script that injects a new action on the menu to send mail with the receipt.
 // @author       Fabricio Oliveira Silva - fauosilva@gmail.com
 // @match        https://gestaoclick.com/movimentacoes_financeiras/index_recebimento*
 // @updateURL    https://raw.githubusercontent.com/fauosilva/CEFV-GestaoClickPublic/master/Source/Tampermonkey/EnviarRecibo.js
+// @downloadURL  https://raw.githubusercontent.com/fauosilva/CEFV-GestaoClickPublic/master/Source/Tampermonkey/EnviarRecibo.js
 // @grant        none
 // ==/UserScript==
 
